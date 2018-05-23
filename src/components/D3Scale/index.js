ff57e4e10497f30cf0ee88eaeb7146e2bb98ffe3
unknown <wjf563745940@163.com>
2018-05-23 20:27:07 +0800

    '5'

--- src/components/D3Scale/index.js
+++ src/components/D3Scale/index.js
@@ -1,11 +1,13 @@
 import Rect, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Tabs,Card } from 'antd';
-import Statistics from './Statistics';
-import Search from './Search';
-import Transformations from './Transformations';
-import Nest from './Nest';
-
+import ScaleLine from './ScaleLine'
+import ScaleLog from './ScaleLog'
+import ScalePow from './ScalePow'
+import Quantize from './Quantize'
+import Quantile from './Quantile'
+import Threshold from './Threshold'
+import Ordinal from './Ordinal'
 import style from './index.less'
 
 export default class D3Array extends Component {
@@ -32,9 +34,26 @@ export default class D3Array extends Component {
         
                     <Tabs defaultActiveKey="1" onChange={callback}>
                         <TabPane tab="线性" key="1" className={style.leftPanel}>
-                            xianxing
+                            <ScaleLine></ScaleLine>
                         </TabPane>
-                        
+                        <TabPane tab="指数" key="2" className={style.leftPanel}>
+                            <ScalePow></ScalePow>
+                        </TabPane>
+                        <TabPane tab="对数" key="3" className={style.leftPanel}>
+                            <ScaleLog></ScaleLog>
+                        </TabPane>
+                        <TabPane tab="量子" key="4" className={style.leftPanel}>
+                            <Quantize></Quantize>
+                        </TabPane>
+                        <TabPane tab="分位" key="5" className={style.leftPanel}>
+                            <Quantile></Quantile>
+                        </TabPane>
+                        <TabPane tab="阀值" key="6" className={style.leftPanel}>
+                            <Threshold></Threshold>
+                        </TabPane>
+                        <TabPane tab="序数" key="7" className={style.leftPanel}>
+                            <Ordinal></Ordinal>
+                        </TabPane>
                     </Tabs>
                
             </div>
